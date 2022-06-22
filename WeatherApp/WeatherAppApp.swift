2769fcb3b492b6d0d863c097f6b9c5ae3aa8ff77
hgtucel <tucelhg@gmail.com>
2022-06-22 20:24:24 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Gürkan Tüçel on 22.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
